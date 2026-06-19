e05417fb30b794576f647863f9a1b07010e2bc1b
Roshan <3264rkg@gmail.com>
2026-06-19 23:57:29 +0530

    third commit

--- main.py
+++ main.py
@@ -1,68 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from io import StringIO
-import traceback
-import sys
-import re
+import csv
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
 
-class CodeRequest(BaseModel):
-    code: str
+students_data = []
 
-def execute_python_code(code: str):
-    old_stdout = sys.stdout
-    captured_output = StringIO()
-    sys.stdout = captured_output
+with open("q-fastapi.csv", newline="") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        students_data.append({
+            "studentId": int(row["studentId"]),
+            "class": row["class"]
+        })
 
-    try:
-        exec(code)
+@app.get("/api")
+async def get_students(class_: list[str] = Query(default=None, alias="class")):
 
-        return {
-            "success": True,
-            "output": captured_output.getvalue()
-        }
-
-    except Exception:
-        return {
-            "success": False,
-            "output": traceback.format_exc()
-        }
-
-    finally:
-        sys.stdout = old_stdout
-
-@app.post("/code-interpreter")
-async def code_interpreter(request: CodeRequest):
-
-    result = execute_python_code(request.code)
-
-    if result["success"]:
-        return {
-            "error": [],
-            "result": result["output"]
-        }
-
-    tb = result["output"]
-
-    
-    matches = re.findall(r'File "<string>", line (\d+)', tb)
-
-    if matches:
-        error_lines = [int(matches[-1])]
-    else:
-        error_lines = []
+    if class_ is None:
+        return {"students": students_data}
 
     return {
-        "error": error_lines,
-        "result": tb
+        "students": [
+            s for s in students_data
+            if s["class"] in class_
+        ]
     }
